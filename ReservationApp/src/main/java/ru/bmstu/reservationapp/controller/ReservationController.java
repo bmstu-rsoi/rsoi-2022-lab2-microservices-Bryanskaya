db933c7bc3baf1ae8ac94d86a6624c80eae74fa3
Bryanskaya <bryanskayakatya@yandex.ru>
2022-11-02 00:29:05 +0300

    UPD: url

--- ReservationApp/src/main/java/ru/bmstu/reservationapp/controller/ReservationController.java
+++ ReservationApp/src/main/java/ru/bmstu/reservationapp/controller/ReservationController.java
@@ -5,14 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.bmstu.reservationapp.dto.ReservationResponse;
+import org.springframework.web.bind.annotation.*;
 import ru.bmstu.reservationapp.service.ReservationService;
 
-import javax.websocket.server.PathParam;
 import java.util.UUID;
 
 @Slf4j
@@ -24,21 +19,21 @@ public class ReservationController {
 
     @GetMapping(produces = "application/json")
     public ResponseEntity<?> getReservationsByUsername(@RequestHeader(value = "X-User-Name") String username) {
-        log.info(">>> RESERVATION: Request to get all reservations by username was caught.");
+        log.info(">>> RESERVATION: Request to get all reservations by username={} was caught.", username);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(reservationService.getReservationsByUsername(username));
     }
 
-    @GetMapping(produces = "application/json")
+    @GetMapping(value = "/{reservationUid}", produces = "application/json")
     public ResponseEntity<?> getReservationsByUsernameReservationUid(@RequestHeader(value = "X-User-Name") String username,
-                                                                     @PathParam(value = "reservationUid") String reservationUId) {
+                                                                     @PathVariable(value = "reservationUid") String reservationUid) {
         log.info(">>> RESERVATION: Request to get all reservations by username and reservationUid was caught.");
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(reservationService.getReservationsByUsernameReservationUid(username,
-                        UUID.fromString(reservationUId)));
+                        UUID.fromString(reservationUid)));
     }
 }
