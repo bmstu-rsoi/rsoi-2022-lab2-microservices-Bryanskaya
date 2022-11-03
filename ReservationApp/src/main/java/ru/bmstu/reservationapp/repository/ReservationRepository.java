afec55bdf34cb3463ca724805735d40703daa628
Bryanskaya <bryanskayakatya@yandex.ru>
2022-11-03 22:11:31 +0300

    FIX: bug in reservation query
    Query of getting all reservations by username and reservation_uid

--- ReservationApp/src/main/java/ru/bmstu/reservationapp/repository/ReservationRepository.java
+++ ReservationApp/src/main/java/ru/bmstu/reservationapp/repository/ReservationRepository.java
@@ -2,10 +2,12 @@ package ru.bmstu.reservationapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import ru.bmstu.reservationapp.model.ReservationEntity;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,7 +16,7 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
             nativeQuery = true)
     List<ReservationEntity> getReservationsByUsername(String username);
 
-    @Query(value = "SELECT * FROM reservation WHERE username = ?1 and reservationUid = ?2",
+    @Query(value = "SELECT * FROM reservation WHERE username = ?1 and reservation_uid = ?2",
             nativeQuery = true)
     ReservationEntity getReservationsByUsernameReservationUid(String username, UUID reservationUid);
 }
